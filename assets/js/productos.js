e605f995d3b8ffb993d1fb61e869b21c3e8f21d8
yatnla <yatnlayatn@gmail.com>
2026-06-24 01:24:22 -0300

    creación del repositorio

---
+++ assets/js/productos.js
@@ -0,0 +1,58 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "My City is Yours",
+        desc: "Cao Fei, 2025",
+        precio: 28500,
+        img: "my-city-is-yours.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Still Life",
+        desc: "Charles Negre, 2020",
+        precio: 32000,
+        img: "still-life.webp"
+    },
+    {
+        id: 3,
+        nombre: "Eastern & Oriental",
+        desc: "Stefanie Moshammer, 2023",
+        precio: 35900,
+        img: "eastern-and-oriental.webp"
+    },
+    {
+        id: 4,
+        nombre: "Reducción",
+        desc: "Adrián Galindo, 2025",
+        precio: 24000,
+        img: "reduccion.jpg"
+    },
+    {
+        id: 5,
+        nombre: "Promise Land",
+        desc: "Greggory Jones, 2021",
+        precio: 29900,
+        img: "promise-land.webp"
+    },
+    {
+        id: 6,
+        nombre: "Conversaciones",
+        desc: "Piedra Ediciones, 2026",
+        precio: 21500,
+        img: "conversaciones.jpg"
+    },
+    {
+        id: 7,
+        nombre: "Flower Smuggler",
+        desc: "Diana Tamane, 2020",
+        precio: 33500,
+        img: "flower-smuggler.webp"
+    },
+    {
+        id: 8,
+        nombre: "Mount Nelson",
+        desc: "Rosie Marks, 2024",
+        precio: 27000,
+        img: "mount-nelson.webp"
+    }
+];
